dbcef0f32b2ef28b655057239723d0cce676399f
bambi teague <teeegsa@gmail.com>
2020-11-09 20:46:50 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+
+  attr_accessor :title, :author
+
+  def initialize
+    @title = title
+    @artist = author
+  end
+
+  def title
+    @title
+  end
+
+  def artist
+    @author
+  end
+
+end 
---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+
+  attr_accessor :title, :artist
+
+  def initialize
+    @title = title
+    @artist = artist
+  end
+
+  def title
+    @title
+  end
+
+  def artist
+    @artist
+  end
+
+end 
